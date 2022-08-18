9773c8d30d3802bf999520e56fc9dba417cc7716
Raphael Matias <raphaelmatias135790@gmail.com>
2022-08-18 13:19:21 -0300

    Adicionar arquivos de projeto.

---
+++ Pigeonhole.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PigeonholeSort
+{
+    internal class Pigeonhole
+    {
+        public static void Sort(int[] array)
+        {
+            int min = array[0];
+            int max = array[0];
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] < min)
+                {
+                    min = array[i];
+                }
+                if (array[i] > max)
+                {
+                    max = array[i];
+                }
+            }
+            int range = max - min + 1;
+            int[] holes = new int[range];
+            for (int i = 0; i < array.Length; i++)
+            {
+                holes[array[i] - min]++;
+            }
+            int index = 0;
+            for (int i = 0; i < range; i++)
+            {
+                while (holes[i]-- > 0)
+                {
+                    array[index++] = i + min;
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿int[] array = { 8, 3, 5, 7, 10, 3, 6, 9, 5, 8, 3 };
+PigeonholeSort.Pigeonhole.Sort(array);
+
+foreach (int i in array)
+{
+    Console.Write(i + " ");
+}
